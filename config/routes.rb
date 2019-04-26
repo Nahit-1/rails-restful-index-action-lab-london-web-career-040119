4abd256c54fd7476c456f61208d6c20381e3fdac
Nahit-1 <nahit86@outlook.com>
2019-04-26 08:26:27 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # get 'index', to: 'students#index' refactored below... --> 
+
+    resources :students, only: :index 
 end
